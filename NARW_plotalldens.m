a03d9483edbb8bb3ced35a6cfba140e338891589
julievdh <julievdh@mit.edu>
2018-08-15 18:49:13 +0200

    change counter variables for loops

--- NARW_plotalldens.m
+++ NARW_plotalldens.m
@@ -6,17 +6,17 @@ load(['/Users/julievanderhoop/Dropbox (Personal)/tag/tagdata/' tag '_flowspeed.m
 % calculate dive duration
 ddur = T(:,2)-T(:,1);
 if exist('dive','var')
-for i = 1:length(dive)
-    dcue = T(i,1):T(i,2); % cues for that dive
+for j = 1:length(dive)
+    dcue = T(j,1):T(j,2); % cues for that dive
     c = get(gca,'colororder'); % get color order
     c = repmat(c,ceil(size(T,1)/size(c,1)),1);    % repeat color order for number of dives
     
-    if isempty(dive(i).stops) == 0
+    if isempty(dive(j).stops) == 0
         
         figure(18),
         subplot(2,2,1), hold on
-        dive(i).clearingtime = [dive(i).stops(:,2)-dive(i).stops(:,1)]';
-        plot(ddur(i),mean(dive(i).clearingtime),'o','color',c(i,:))
+        dive(j).clearingtime = [dive(j).stops(:,2)-dive(j).stops(:,1)]';
+        plot(ddur(j),mean(dive(j).clearingtime),'o','color',c(j,:))
         xlabel('Dive Duration (sec)'), ylabel('Inter-Pause Interval (sec)')
         
         subplot(2,2,2), hold on
@@ -26,26 +26,26 @@ for i = 1:length(dive)
             volrate = gape*speed;
             bolus = 0.5; % half a kg
             densities = 0.001:0.001:0.01;
-            for j = 1:length(volrate)
-                cleartime(j,:) = bolus./densities/volrate(j);
+            for v = 1:length(volrate)
+                cleartime(v,:) = bolus./densities/volrate(v);
             end
             cv = viridis(size(cleartime,2)); % color by densities
-            for j = 1:size(cleartime,2)
-                plot(speed, cleartime(:,j),'color',cv(j,:))
+            for v = 1:size(cleartime,2)
+                plot(speed, cleartime(:,v),'color',cv(v,:))
             end
             xlabel('Swimming Speed (m/s)'), ylabel('Clearing Time (sec)')
             C = regexp(sprintf('density=%.3f#', densities), '#', 'split');
             % legend(C)
         end
-        plot(dive(i).mnspeedperblock,dive(i).stops(:,2)-dive(i).stops(:,1),'o','color',c(i,:))
+        plot(dive(j).mnspeedperblock,dive(j).stops(:,2)-dive(j).stops(:,1),'o','color',c(j,:))
         
         
         % estimate density 
-        for k = 1:size(dive(i).stops,1)
-        dive(i).dens(k) = 0.5/(dive(i).clearingtime(k)*dive(i).vperblock(k));
+        for k = 1:size(dive(j).stops,1)
+        dive(j).dens(k) = 0.5/(dive(j).clearingtime(k)*dive(j).vperblock(k));
         end
         subplot(2,2,3), hold on 
-        plot(dive(i).stops(:,1),dive(i).dens,'color',c(i,:))
+        plot(dive(j).stops(:,1),dive(j).dens,'color',c(j,:))
         % plot pitch track
          
         % pause
