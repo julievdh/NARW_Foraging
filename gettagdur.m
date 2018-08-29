a9f44df1f57f531ac18b8b1f5846425b81825e35
julievdh <julievdh@mit.edu>
2018-08-29 21:27:30 +0200

    first commit: get tag duration from sensor data

---
+++ gettagdur.m
@@ -0,0 +1,8 @@
+function dur = gettagdur(tag)
+% get tag duration from sensor data
+% input is tag id 
+% output is duration of sensor data from deployment, in seconds 
+% julie van der Hoop August 2018 
+loadprh(tag,'p','fs)
+
+dur = length(p)/fs; 
