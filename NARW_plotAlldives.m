b5345548491eca3811f8d3f7b33b2508dc48a8f7
julievdh <julievdh@mit.edu>
2018-09-01 13:14:01 +0200

    many figure changes for MS

--- NARW_plotAlldives.m
+++ NARW_plotAlldives.m
@@ -1,30 +1,21 @@
 % list of deployments
-% tags = {'eg05_219a'; 'eg05_224a'; 'eg05_230a'; 'eg05_231a';'eg05_231b';'eg05_232a';'eg05_235a';
-%    'eg02_213b';'eg02_213g';'eg02_220f';'eg02_221d';'eg02_222c';'eg02_229e';'eg02_232d';
-%    'eg02_233a';'eg02_236c'};%'eg05_218c'};
 close all
 % tags is deployment name, time of tag on, cue of tag off
 load('NARW_foraging_tags')
 %%
-for i = 10 %:length(tags)
+allddur = nan(10,40); allnstops = nan(10,40);
+mnboutdur = nan(10,40); 
+for i = 1:length(tags)
     tag = tags{i};
     loadprh(tag);
     
     % cut depth to time of tag off
     %p = p(1:tags{i,3});
     t = (1:length(p))/fs/3600; % compute time vector
-    
-    % compute amount of time within 10m of surface
-    %[tags{i,4},tags{i,5}] = findsurftime(p,fs,10);
-    %figure(5), subplot(length(tags),1,i), hold on
-    %histogram(tags{i,5}/60,0:1:80), xlabel('Continuous Surface Time (min)')
-    
-    %figure(6), hold on
-    %cdfplot(tags{i,5}/60)
-    
+       
     % how many dives > 50 m
     T = finddives(p,fs,50,1);
-    % mean(T(:,2)-T(:,1))/60
+    ddur = T(:,2)-T(:,1); % calculate duration of all dives
     
     % import flow speed for the tag
     load(['/Users/julievanderhoop/Dropbox (Personal)/tag/tagdata/' tag '_flowspeed.mat'])
@@ -39,15 +30,18 @@ for i = 10 %:length(tags)
         end
     end
     xlabel('Duration of fluking bout (sec)'), ylabel('Volume filtered m^3')
-    
+   
     % plot by time of day
-    figure(100), subplot(length(tags),1,i), hold on, box on
+    figure(100), subplot('position',[0.13 1.01-(i*0.095) 0.8 0.08]), hold on, box on
     UTC = tags{i,2}(4:end);
     hh=plot((UTC(1)+UTC(2)/60+UTC(3)/3600)+[1:length(p)]/fs/3600,-p,'k','LineWidth',1);
     ylim([-200 10]), xlim([9 24])
-    
-    figure(101), subplot(length(tags),1,i), hold on, box on
-    plot(t,-p,'k'), ylim([-200 10]), xlim([0 11.7]) % xmax of tags
+    set(gca,'ytick',[-150 -50 0],'yticklabels',[150 50 0]), plot([0 24],[-50 -50],':','color',[0.5 0.5 0.5])
+    text(9.5,-150,regexprep(tag(3:end),'_','-'))
+    if i <= 9, set(gca,'xtick',[]), end
+   
+   % figure(101), subplot(length(tags),1,i), hold on, box on
+   % plot(t,-p,'k'), ylim([-200 10]), xlim([0 11.7]) % xmax of tags
     
     NARW_divepauseplot
     
@@ -55,16 +49,12 @@ for i = 10 %:length(tags)
         if isempty(dive(k).stops) == 0
             figure(11), subplot(2,2,1), hold on
             plot(ddur(k)/60,size(dive(k).stops,1),'o')
+            allddur(i,k) = ddur(k); allnstops(i,k) = size(dive(k).stops,1);  % store those values
             xlabel('Dive Duration (min)'), ylabel('Number of fluking bouts')
             subplot(2,2,2), hold on
             errorbar(ddur(k)/60,mean([dive(k).clearingtime]),std([dive(k).clearingtime]),'o')
+            mnboutdur(i,k) = mean([dive(k).clearingtime]); 
             xlabel('Dive Duration (min)'), ylabel('Duration of fluking bouts (sec)')
-            %subplot(2,2,3), hold on
-            %plot(ddur(k)/60,tags{i,6},'o')
-            %xlabel('Dive Duration (min)'), ylabel('Age (years)')
-            %subplot(2,2,4), hold on
-            %errorbar(tags{i,6},mean([dive(k).vperblock]),std([dive(k).vperblock]),'o')
-            %xlabel('Age (years)'), ylabel('Volume filtered per fluking interval (m^3)')
         end
         
         
@@ -108,22 +98,38 @@ for i = 10 %:length(tags)
     plot(t,-p/100,'k')
     
     % pause
-    
+    % save(['/Users/julievanderhoop/Dropbox (Personal)/tag/tagdata/' tag '_flowspeed.mat'],'dive','-append')
     if i < 10
         clear p ptrack pitch ph Aw Mw fs % to remove carry-over of variables
     end
 end
 
+figure(11), 
+subplot(2,2,3), hold on 
+plot(alldepth,allvperdive,'o')
+xlabel('Maximum Dive Depth (m)'), ylabel('Total Volume Filtered (m^3)')
+subplot(2,2,4), hold on 
+plot(alldur,allvperdive,'o')
+xlabel('Dive Duration (sec)'), ylabel('Total Volume Filtered (m^3)')
+
+
 return
+
+%% stats
+lm1 = fitlm(allddur(~isnan(allddur)),allnstops(~isnan(allnstops)))
+plot(lm1)
+
+lm2 = fitlm(allddur(~isnan(allddur)),mnboutdur(~isnan(mnboutdur)))
+plot(lm2)
 
 
 figure(100), xlabel('Local Time'), adjustfigurefont
 set(gcf,'position',[323 61 512 612],'paperpositionmode','auto')
 print('NARW_alldives_TOD.png','-dpng','-r300')
 
-figure(101), xlabel('Hours since tag on'), adjustfigurefont
-set(gcf,'position',[323 61 512 612],'paperpositionmode','auto')
-print('NARW_alldives_TSTO.png','-dpng','-r300')
+% figure(101), xlabel('Hours since tag on'), adjustfigurefont
+% set(gcf,'position',[323 61 512 612],'paperpositionmode','auto')
+% print('NARW_alldives_TSTO.png','-dpng','-r300')
 
 % max(vertcat(tags{:,5}))/60 = longest surface time above 10 m
 
