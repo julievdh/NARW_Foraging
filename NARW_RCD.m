425fa73ec943586167a7dbab111e04090dda68bf
julievdh <julievdh@mit.edu>
2018-09-07 07:45:25 -0400

    change to circular statistics

--- NARW_RCD.m
+++ NARW_RCD.m
@@ -4,7 +4,9 @@
 
 % load prh
 load('NARW_foraging_tags')
-ID = 8; loadprh(tags{ID})
+for i = 1%clos:length(tags)
+    ID = i; 
+loadprh(tags{ID})
 
 % downsample everything to every 10 seconds 
 [pf,qf] = rat(0.1/fs,0.0001); % obtain resampling factors
@@ -13,9 +15,9 @@ Mw_rs = resample(Mw,pf,qf);
 [pitch_rs,roll_rs] = a2pr(Aw_rs); 
 head_rs = m2h(Mw_rs,pitch_rs,roll_rs); 
 
-figure, hold on
-plott(head_rs,0.1,'.') % downsampled and recomputed data
-plott(head,fs) % original data
+%figure, hold on
+%plott(head_rs,0.1,'.') % downsampled and recomputed data
+%plott(head,fs) % original data
 
 % diff 
 myDiff=diff(head_rs); 
@@ -29,23 +31,27 @@ t_rs = (1:length(head_rs))/0.1; % time vector for downsampled series
 for i = 1:size(T,1)
     st = nearest(t_rs',T(i,1));
     ed = nearest(t_rs',T(i,2)); 
-    d_RCD(i,1:2) = [mean(myDiff(st:ed)) std(myDiff(st:ed))]; % mean/STD RCD in dive
-    figure(3), rose(myDiff(st:ed))
+    d_RCD(i,1:2) = [circ_mean(myDiff(st:ed)) circ_std(myDiff(st:ed))]; % mean/STD RCD in dive
+    figure(3), hold on, rose(myDiff(st:ed))
 end
 for i = 1:size(T,1)-1
     st = nearest(t_rs',T(i,2)); % end of one dive
     ed = nearest(t_rs',T(i+1,1)); % start of next dive
-    s_RCD(i,1:2) = [mean(myDiff(st:ed)) std(myDiff(st:ed))]; % mean/STD RCD at surface
-    figure(4), rose(myDiff(st:ed))
+    s_RCD(i,1:2) = [circ_mean(myDiff(st:ed)) circ_std(myDiff(st:ed))]; % mean/STD RCD at surface
+    figure(4), hold on, rose(myDiff(st:ed))
 end
 
 % plot pitch track with coloured RCD just to check 
-figure
+figure(1), clf
 xx = [ptrack(1:50:end-50,1) ptrack(1:50:end-50,1)]; % x coords, downsampled
 yy = [ptrack(1:50:end-50,2) ptrack(1:50:end-50,2)]; % y coords, downsampled
 zz = zeros(size(xx));
 cc = [myDiff myDiff];
 hs = surf(xx,yy,zz,cc,'EdgeColor','Interp','LineWidth',3);
 view(2), colorbar, caxis([-pi/4 pi/4])
-    
-    
+   
+
+
+end
+
+% right now s_RCD and d_RCD are mean and SD RCD per dive 
