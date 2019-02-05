16d3c499d22624805b7d685b43b465edc255febf
julievdh <julievdh@mit.edu>
2019-02-05 15:39:05 +0100

    first commit

---
+++ FunEcol_RevisionCalcs.m
@@ -0,0 +1,8 @@
+% Functional Ecology Revision Calculations
+
+% How much volume is 9 million copepods? 
+% one copepod = 2 - 4 mm^3
+cop_vol_low = 9E6*2;
+cop_vol_high = 9E6*4;
+% convert mm^3 to m^3 
+
