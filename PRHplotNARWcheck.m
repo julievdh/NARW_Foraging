9ecdd1f39222b485b819a9663ea6fd3fd1c07b22
julievdh <julievdh@mit.edu>
2019-03-12 15:49:08 +0100

    show prh instead of acc

--- PRHplotNARWcheck.m
+++ PRHplotNARWcheck.m
@@ -1,4 +1,12 @@
+load('NARW_foraging_tags')
+ID = 8;
+tag = tags{ID};
+loadprh(tag);
+t = (1:length(p))/fs;
+
 MSA = msa(Aw);
+[v,ph,mx,fr] = findflukes(Aw,Mw,fs,0.3,0.02,[2 8]); % calculate pitch deviation
+
 figure(31), clf
 ax(1) = subplot(411);
 plot(t/60,MSA), ylabel('MSA (g)'), grid on
@@ -6,11 +14,11 @@ ax(2) = subplot(412);
 plot(t/60,ph), ylabel('Pitch Deviation (deg)'), grid on
 ylim([-0.25 0.25])
 ax(3) = subplot(413);
-plot(t/60,Aw), ylabel('Acceleration (g)'), grid on
+plot(t/60,rad2deg([pitch roll head])), ylabel('Orientation (deg)'), grid on
 ax(4) = subplot(414);
 plot(t/60,-p), ylabel('Depth (m)'), grid on
 linkaxes(ax,'x')
-xlim([606 664]), xlabel('Time (min)')
+xlim([606 648]), xlabel('Time (min)')
 
-adjustfigurefont
+adjustfigurefont('Helvetica',14)
 print('NARW-PRH-depth.png','-dpng','-r90')
