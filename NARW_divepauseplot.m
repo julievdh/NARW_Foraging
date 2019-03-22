a274ecf848150011629a36866e67c20ba2079176
julievdh <julievdh@mit.edu>
2019-03-22 11:39:00 -0600

    change pseudotracks to include estimated speed

--- NARW_divepauseplot.m
+++ NARW_divepauseplot.m
@@ -40,37 +40,40 @@ for j = [9 10 11 12]
     plot3(flowtrack(end,1)-startpt(1),flowtrack(end,2)-startpt(2),-p(round(dcue(end)*fs)),'k^','markerfacecolor','k','MarkerSize',10)
     
     % plot pitch
-    [v,ph,mx,fr] = findflukes(Aw,Mw,fs,0.3,0.02,[2 8]); % calculate pitch deviation
-    plot3(flowtrack(:,1)-startpt(1),flowtrack(:,2)-startpt(2),ph(round(dcue*fs))*60-p(round(dcue*fs)),'k')
+%    [v,ph,mx,fr] = findflukes(Aw,Mw,fs,0.3,0.02,[2 8]); % calculate pitch deviation
+%    plot3(flowtrack(:,1)-startpt(1),flowtrack(:,2)-startpt(2),ph(round(dcue*fs))*60-p(round(dcue*fs)),'k')
     
     if isempty(dive(j).stops) == 0
         for k = 1:length(dive(j).stops)
-            plot3(flowtrack(round(dive(j).stops(k,1))-dcue(1),1)-startpt(1),flowtrack(round(dive(j).stops(k,1))-dcue(1),2)-startpt(2),-p(round(dive(j).stops(k,1)*fs))+8,'kv')
+            plot3(flowtrack(round(dive(j).stops(k,1)-dcue(1)),1)-startpt(1),flowtrack(round(dive(j).stops(k,1)-dcue(1)),2)-startpt(2),-p(round(dive(j).stops(k,1)*fs))+8,'kv')
         end
     end
     view([17 27]), grid on, % pause
     xlabel('Easting (m)'), ylabel('Northing (m)'), zlabel('Depth (m)'), rotate_labels(gca)
     set(gca,'xtick',-400:200:400,'ytick',-400:200:400)
     
-    tort(:,j) = nanmean(tortuosity(flowtrack(dive(j).btm),:),fs,10,1);
+    tort(:,j) = nanmean(tortuosity(flowtrack(dive(j).btm,1:2),fs,10));
     % nanmean(tort)
 end
 
 adjustfigurefont('Helvetica',14)
-subplot(221), xlim([-250 250]), ylim([0 500]), zlim([-150 10]), title('A','FontSize',18,'FontWeight','bold')
+subplot(221), xlim([-50 10]), ylim([-10 50]), zlim([-150 10]), title('A','FontSize',18,'FontWeight','bold')
 axletter(gca,['vol: ' num2str(round(sum(dive(9).vperblock),0)) ' m^3'],14,0.9)
 axletter(gca,['tortuosity ' num2str(round(tort(1,9),3))],14,0.9,0.7)
 
-subplot(222), xlim([-100 400]), ylim([-400 100]), zlim([-150 10]), title('B','FontSize',18,'FontWeight','bold')
+subplot(222), xlim([0 60]), ylim([-20 40]), zlim([-150 10]), title('B','FontSize',18,'FontWeight','bold')
 axletter(gca,['vol: ' num2str(round(sum(dive(10).vperblock),0)) ' m^3'],14,0.9)
 axletter(gca,['tortuosity ' num2str(round(tort(1,10),3))],14,0.9,0.7)
 
-subplot(223), xlim([-150 350]), ylim([-350 150 ]), zlim([-150 10]), title('C','FontSize',18,'FontWeight','bold')
+subplot(223), xlim([-10 50]), ylim([-50 10]), zlim([-150 10]), title('C','FontSize',18,'FontWeight','bold')
 axletter(gca,['vol: ' num2str(round(sum(dive(11).vperblock),0)) ' m^3'],14,0.9)
 axletter(gca,['tortuosity ' num2str(round(tort(1,11),3))],14,0.9,0.7)
 
-subplot(224), xlim([-450 50]), ylim([-150 350]), zlim([-150 10]), title('D','FontSize',18,'FontWeight','bold')
+subplot(224), xlim([-50 10]), ylim([-10 50]), zlim([-150 10]), title('D','FontSize',18,'FontWeight','bold')
 axletter(gca,['vol: ' num2str(round(sum(dive(12).vperblock),0)) ' m^3'],14,0.9)
 axletter(gca,['tortuosity ' num2str(round(tort(1,12),3))],14,0.9,0.7)
-colorbar('position',[0.93 0.11 0.02 0.815])
-% print('NARW_ptrack4.png','-dpng','-r300')
+
+h = colorbar('position',[0.93 0.11 0.02 0.815])
+ylabel(h,'Speed (m/s)')
+
+print('NARW_ptrack4.png','-dpng','-r300')
